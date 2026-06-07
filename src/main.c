5a8e9707ae9557baedca7726ad44f1a632a351f8
syswraith <syswraith@proton.me>
2026-06-07 19:36:36 +0530

    reading instructions from memory and fetch decode execute

--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
 #include "../include/chip8.h"
 #include "../include/memory.h"
+#include "../include/opcode.h"
+#include <stdint.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
   (void)argc;
 
   chip_init();
   load_rom(argv[1]);
-  dump_memory();
-  dump_registers();
-  dump_stack();
+
+  while (1) {
+    uint16_t instruction =
+        (chip.memory[chip.pc] << 8) | chip.memory[chip.pc + 1];
+
+    printf("Executing %04X\n", instruction);
+    if (instruction == 0x0000) {
+      break;
+    }
+
+    fetch(&instruction);
+  }
 
   return 0;
 }
