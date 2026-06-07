302c3772e80f646fdd8c6b89b8260be92164a51c
syswraith <syswraith@proton.me>
2026-06-07 19:00:14 +0530

    added jump, subroutine_call, ret instructions

--- include/opcode.h
+++ include/opcode.h
@@ -3,7 +3,7 @@
 typedef struct {
   uint16_t mask;
   uint16_t pattern;
-  void (*handler)(uint16_t instruction);
+  void (*handler)(const uint16_t *instruction);
 } Opcode;
 
 void get_n(const uint16_t *instruction, uint16_t *n);
@@ -11,3 +11,10 @@ void get_nn(const uint16_t *instruction, uint16_t *nn);
 void get_nnn(const uint16_t *instruction, uint16_t *nn);
 void get_x(const uint16_t *instruction, uint16_t *x);
 void get_xy(const uint16_t *instruction, uint16_t *x, uint16_t *y);
+
+// general format 0x1NNN
+void jump(const uint16_t *instruction);
+void subroutine_call(const uint16_t *instruction);
+
+void fetch(const uint16_t *instruction);
+void decode_and_execute(const uint16_t *instruction, const uint16_t index);
--- src/opcode.c
+++ src/opcode.c
@@ -1,8 +1,9 @@
 #include "../include/opcode.h"
+#include "../include/chip8.h"
+#include "../include/stack.h"
 #include <assert.h>
 #include <stdint.h>
-
-Opcode opcode[] = {};
+#include <stdio.h>
 
 void get_n(const uint16_t *instruction, uint16_t *n) {
   assert(n);
@@ -30,3 +31,49 @@ void get_xy(const uint16_t *instruction, uint16_t *x, uint16_t *y) {
   *x = ((*instruction & 0x0f00) >> 8);
   *y = ((*instruction & 0x00f0) >> 4);
 }
+
+void jump(const uint16_t *instruction) {
+  uint16_t nnn;
+  get_nnn(instruction, &nnn);
+
+  printf("Executing jump\n");
+
+  chip.pc = nnn;
+}
+
+void subroutine_call(const uint16_t *instruction) {
+  uint16_t nnn;
+  get_nnn(instruction, &nnn);
+
+  printf("Pushing onto stack and setting pc\n");
+
+  push(chip.pc + 2);
+  chip.pc = nnn;
+}
+
+void ret(const uint16_t *instruction) {
+  (void)instruction;
+  printf("Returning to previous address\n");
+  chip.pc = pop();
+}
+
+Opcode opcode[] = {
+    // pls no separate
+
+    {0xf000, 0x1000, jump},
+    {0xf000, 0x2000, subroutine_call},
+    {0x00ff, 0x00ee, ret}};
+
+void fetch(const uint16_t *instruction) {
+  for (uint16_t index = 0; index < size(opcode); index++) {
+    decode_and_execute(instruction, index);
+  }
+};
+
+void decode_and_execute(const uint16_t *instruction, const uint16_t index) {
+  if ((*instruction & opcode[index].mask) == opcode[index].pattern) {
+    printf("Matched with pattern: %02X\n", opcode[index].pattern);
+    opcode[index].handler(instruction);
+    return;
+  }
+};
