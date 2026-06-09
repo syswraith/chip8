21b6940d31e7b2b2aa19d4bac16499eae3673801
syswraith <syswraith@proton.me>
2026-06-09 12:41:11 +0530

    implement additional CHIP-8 instructions and update opcode helper functions to use 8-bit types

--- include/opcode.h
+++ include/opcode.h
@@ -7,11 +7,11 @@ typedef struct {
 } Opcode;
 
 // helper functions
-void get_n(const uint16_t *instruction, uint16_t *n);
-void get_nn(const uint16_t *instruction, uint16_t *nn);
-void get_nnn(const uint16_t *instruction, uint16_t *nn);
-void get_x(const uint16_t *instruction, uint16_t *x);
-void get_xy(const uint16_t *instruction, uint16_t *x, uint16_t *y);
+void get_n(const uint16_t *instruction, uint8_t *n);
+void get_nn(const uint16_t *instruction, uint8_t *nn);
+void get_nnn(const uint16_t *instruction, uint16_t *nnn);
+void get_x(const uint16_t *instruction, uint8_t *x);
+void get_xy(const uint16_t *instruction, uint8_t *x, uint8_t *y);
 
 /*
  * General format
@@ -36,12 +36,24 @@ void if_vx_ee_nn(const uint16_t *instruction);
 void if_vx_ne_nn(const uint16_t *instruction);
 void if_vx_ee_vy(const uint16_t *instruction);
 void vx_eq_nn(const uint16_t *instruction);
-void vx_p_eq_nn(const uint16_t *instruction);
+void vx_add_eq_nn(const uint16_t *instruction);
 void vx_eq_vy(const uint16_t *instruction);
-void vx_p_eq_vy(const uint16_t *instruction);
+void vx_add_eq_vy(const uint16_t *instruction);
 void vx_or_eq_vy(const uint16_t *instruction);
 void vx_and_eq_vy(const uint16_t *instruction);
 void vx_xor_eq_vy(const uint16_t *instruction);
+
+void vx_add_eq_vy_carry(const uint16_t *instruction);
+void vx_sub_eq_vy_borrow(const uint16_t *instruction);
+void vx_shr_eq_vy(const uint16_t *instruction);
+void vx_rev_sub_eq_vy_borrow(const uint16_t *instruction);
+void vx_shl_eq_vy(const uint16_t *instruction);
+
+void i_eq_nn(const uint16_t *instruction);
+void jump_nnn(const uint16_t *instruction);
+void vx_eq_rand_nn(const uint16_t *instruction);
+
+
 
 void fetch(const uint16_t *instruction);
 void decode_and_execute(const uint16_t *instruction, const uint16_t index);
--- src/opcode.c
+++ src/opcode.c
@@ -2,30 +2,31 @@
 #include "../include/chip8.h"
 #include "../include/stack.h"
 #include <assert.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 
-void get_n(const uint16_t *instruction, uint16_t *n) {
+void get_n(const uint16_t *instruction, uint8_t *n) {
   assert(n);
   *n = (*instruction & 0x00f);
 }
 
-void get_nn(const uint16_t *instruction, uint16_t *nn) {
+void get_nn(const uint16_t *instruction, uint8_t *nn) {
   assert(nn);
   *nn = (*instruction & 0x00ff);
 }
 
-void get_nnn(const uint16_t *instruction, uint16_t *nn) {
-  assert(nn);
-  *nn = (*instruction & 0x0fff);
+void get_nnn(const uint16_t *instruction, uint16_t *nnn) {
+  assert(nnn);
+  *nnn = (*instruction & 0x0fff);
 }
 
-void get_x(const uint16_t *instruction, uint16_t *x) {
+void get_x(const uint16_t *instruction, uint8_t *x) {
   assert(x);
   *x = ((*instruction & 0x0f00) >> 8);
 }
 
-void get_xy(const uint16_t *instruction, uint16_t *x, uint16_t *y) {
+void get_xy(const uint16_t *instruction, uint8_t *x, uint8_t *y) {
   assert(x);
   assert(y);
   *x = ((*instruction & 0x0f00) >> 8);
@@ -59,8 +60,7 @@ void ret(const uint16_t *instruction) {
 
 void if_vx_ee_nn(const uint16_t *instruction) {
 
-  uint16_t nn;
-  uint16_t x;
+  uint8_t nn, x;
   get_x(instruction, &x);
   get_nn(instruction, &nn);
 
@@ -71,8 +71,7 @@ void if_vx_ee_nn(const uint16_t *instruction) {
 
 void if_vx_ne_nn(const uint16_t *instruction) {
 
-  uint16_t nn;
-  uint16_t x;
+  uint8_t nn, x;
   get_x(instruction, &x);
   get_nn(instruction, &nn);
 
@@ -81,11 +80,9 @@ void if_vx_ne_nn(const uint16_t *instruction) {
   }
 }
 
-
-
 void if_vx_ee_vy(const uint16_t *instruction) {
 
-  uint16_t nn, x, y;
+  uint8_t nn, x, y;
   get_xy(instruction, &x, &y);
   get_nn(instruction, &nn);
 
@@ -96,17 +93,16 @@ void if_vx_ee_vy(const uint16_t *instruction) {
 
 void vx_eq_nn(const uint16_t *instruction) {
 
-  uint16_t nn, x;
+  uint8_t nn, x;
   get_x(instruction, &x);
   get_nn(instruction, &nn);
 
   chip.v[x] = nn;
 }
 
+void vx_add_eq_nn(const uint16_t *instruction) {
 
-void vx_p_eq_nn(const uint16_t *instruction) {
-
-  uint16_t nn, x;
+  uint8_t nn, x;
   get_x(instruction, &x);
   get_nn(instruction, &nn);
 
@@ -114,37 +110,117 @@ void vx_p_eq_nn(const uint16_t *instruction) {
 }
 
 void vx_eq_vy(const uint16_t *instruction) {
-  uint16_t x, y;
+  uint8_t x, y;
   get_xy(instruction, &x, &y);
 
   chip.v[x] = chip.v[y];
 }
 
-void vx_p_eq_vy(const uint16_t *instruction) {
-  uint16_t x, y;
+void vx_add_eq_vy(const uint16_t *instruction) {
+  uint8_t x, y;
   get_xy(instruction, &x, &y);
 
   chip.v[x] += chip.v[y];
 }
 
 void vx_or_eq_vy(const uint16_t *instruction) {
-  uint16_t x, y;
+  uint8_t x, y;
   get_xy(instruction, &x, &y);
 
   chip.v[x] |= chip.v[y];
 }
 void vx_and_eq_vy(const uint16_t *instruction) {
-  uint16_t x, y;
+  uint8_t x, y;
   get_xy(instruction, &x, &y);
 
   chip.v[x] &= chip.v[y];
 }
+
 void vx_xor_eq_vy(const uint16_t *instruction) {
-  uint16_t x, y;
+  uint8_t x, y;
   get_xy(instruction, &x, &y);
 
   chip.v[x] ^= chip.v[y];
 }
+
+void vx_add_eq_vy_carry(const uint16_t *instruction) {
+  uint8_t x, y;
+  get_xy(instruction, &x, &y);
+
+  uint16_t sum = chip.v[x] + chip.v[y];
+  uint16_t carry = (sum > 0xff) ? 1 : 0;
+  chip.v[x] = sum & 0xff;
+  chip.v[0xf] = carry;
+}
+
+void vx_sub_eq_vy_borrow(const uint16_t *instruction) {
+  uint8_t x, y;
+  get_xy(instruction, &x, &y);
+
+  uint16_t no_borrow = (chip.v[x] >= chip.v[y]) ? 1 : 0;
+  chip.v[x] -= chip.v[y];
+  chip.v[0xf] = no_borrow;
+}
+
+void vx_shr_eq_vy(const uint16_t *instruction) {
+  uint8_t x, y;
+  get_xy(instruction, &x, &y);
+
+  uint8_t shifted_bit = chip.v[y] & 0x01;
+  chip.v[x] = chip.v[y] >> 1;
+  chip.v[0xf] = shifted_bit;
+}
+
+void vx_rev_sub_eq_vy_borrow(const uint16_t *instruction) {
+    uint8_t x, y;
+    get_xy(instruction, &x, &y);
+    uint8_t no_borrow = (chip.v[y] >= chip.v[x]) ? 1 : 0;
+    chip.v[x] = chip.v[y] - chip.v[x];
+    chip.v[0xF] = no_borrow;
+}
+
+void vx_shl_eq_vy(const uint16_t *instruction) {
+  uint8_t x, y;
+  get_xy(instruction, &x, &y);
+
+  uint8_t shifted_bit = chip.v[y] >> 7;
+  chip.v[x] = chip.v[y] << 1;
+  chip.v[0xf] = shifted_bit;
+}
+
+void if_vx_ne_vy(const uint16_t *instruction) {
+
+  uint8_t nn, x, y;
+  get_xy(instruction, &x, &y);
+  get_nn(instruction, &nn);
+
+  if (chip.v[x] != chip.v[y]) {
+    chip.pc += 2;
+  }
+}
+
+void i_eq_nn(const uint16_t *instruction) {
+  uint8_t nn;
+  get_nn(instruction, &nn);
+
+  chip.index = nn;
+}
+
+void jump_nnn(const uint16_t *instruction) {
+  uint16_t nnn;
+  get_nnn(instruction, &nnn);
+
+  chip.pc = (nnn + chip.v[0x0]);
+}
+
+void vx_eq_rand_nn(const uint16_t *instruction) {
+    uint8_t x, nn, random_number;
+    get_x(instruction, &x);
+    get_nn(instruction, &nn);
+    random_number = rand() % 256;
+    chip.v[x] = random_number & nn;
+}
+
 
 Opcode opcode[] = {
   // pls no separate
@@ -156,16 +232,20 @@ Opcode opcode[] = {
   {0xf000, 0x4000, if_vx_ne_nn}, 
   {0xf000, 0x5000, if_vx_ee_vy},
   {0xf000, 0x6000, vx_eq_nn},
-  {0xf000, 0x7000, vx_p_eq_nn},
+  {0xf000, 0x7000, vx_add_eq_nn},
   {0xf00f, 0x8000, vx_eq_vy},
   {0xf00f, 0x8001, vx_or_eq_vy},
   {0xf00f, 0x8002, vx_and_eq_vy},
   {0xf00f, 0x8003, vx_xor_eq_vy},
-//  {0xf00f, 0x8004, },
-//  {0xf00f, 0x8005, },
-//  {0xf00f, 0x8006, },
-//  {0xf00f, 0x8007, },
-//  {0xf00f, 0x800e, },
+  {0xf00f, 0x8004, vx_add_eq_vy_carry},
+  {0xf00f, 0x8005, vx_sub_eq_vy_borrow},
+  {0xf00f, 0x8006, vx_shr_eq_vy},
+  {0xf00f, 0x8007, vx_rev_sub_eq_vy_borrow},
+  {0xf00f, 0x800e, vx_shl_eq_vy},
+  {0xf00f, 0x9000, if_vx_ne_vy},
+  {0xf000, 0xA000, i_eq_nn},
+  {0xf000, 0xB000, jump_nnn},
+  {0xf000, 0xC000, vx_eq_rand_nn},
 
   // pls no separate
 };
